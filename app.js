787fc4f44513f71534346f80004cd5fad07445cf
Josh <jdoleary91@gmail.com>
2021-09-14 20:50:40 -0400

    refactor: update env

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const userRoutes = require('./routes/users');
 const publicRoutes = require('./routes/sots')
 
 const port = process.env.PORT || 3000;
-const dbURI = process.env.MONGO_URI;
+
 //connecting database
-mongoose.connect('dbURI', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,

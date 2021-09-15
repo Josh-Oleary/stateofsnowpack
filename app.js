defb0fd52b08fad635d2db8e58241e538d8803f8
Josh <jdoleary91@gmail.com>
2021-09-14 20:43:41 -0400

    hide mongoURI

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const publicRoutes = require('./routes/sots')
 
 const port = process.env.PORT || 3000;
 //connecting database
-mongoose.connect("mongodb+srv://sotsAdmin:stateofthesnowpack@cluster0.b8xjv.mongodb.net/sots", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,

f5ee5e42bbe6047d99fe2ef597e252d80ddd71e4
Josh <jdoleary91@gmail.com>
2021-09-14 20:52:57 -0400

    refactor

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const publicRoutes = require('./routes/sots')
 const port = process.env.PORT || 3000;
 
 //connecting database
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://' + process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,

cd3182e0b8c7338bca42baa302ff5abf9065f5dd
Josh <jdoleary91@gmail.com>
2021-11-24 07:51:18 -0500

    mongo store connect attempt

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(session({
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     },
-    store: MongoStore.create({ mogoURL: 'mongodb+srv://sotsAdmin:stateofthesnowpack@cluster0.b8xjv.mongodb.net/sots' })
+    store: new MongoStore({ mogoURL: process.env.MONGO_URL })
 }));
 app.use(flash());
 //authentication middleware

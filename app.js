ef302512ba06ed83ba39f8ce69278db0e641752e
Josh <jdoleary91@gmail.com>
2021-11-24 08:20:57 -0500

    refactor: set up mongo store connection

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ const User = require('./models/user');
 const methodOverride = require('method-override');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const MongoStore = require('connect-mongo');
+const MongoDBStore = require('connect-mongo');
 //declaring external route files
 const adminRoutes = require('./routes/admin')
 const userRoutes = require('./routes/users');
 const publicRoutes = require('./routes/sots')
 
 const port = process.env.PORT || 3000;
-
+const dbUrl = process.env.MONGO_URL
+const secret = process.env.SECRET
 
 mongoose.connect( process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -39,6 +40,31 @@ db.once('open', () => {
 //initializing express app
 const app = express();
 
+
+const store = MongoDBStore.create({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false, 
+    saveUninitialized: true, 
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
 app.use(express.static((path.join(__dirname + '/public'))));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -49,18 +75,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 //configuring session
 app.set('trust proxy', 1)
-app.use(session({
-    resave: false, 
-    saveUninitialized: true, 
-    secret: process.env.SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    },
-    store: new MongoStore({ mogoURL: process.env.MONGO_URL })
-}));
+app.use(session(sessionConfig));
 app.use(flash());
 //authentication middleware
 app.use(passport.initialize());
